297cd93d94fb847af6283398b918ee58b23f2bdb
Alex Ionescu <alxi.2001@gmail.com>
2018-06-13 17:27:39 +0300

    Implement turn taking (Not yet working)

---
+++ Assets/Scripts/Lane.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lane : MonoBehaviour
+{
+    [SerializeField]
+    public Transform Start;
+
+    [SerializeField]
+    public Transform End;
+}
---
+++ Assets/Scripts/TurningCar.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurningCar : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 0.5f;
+
+    Rigidbody _body;
+
+    bool _isTurning;
+
+    Transform _from, _to;
+
+    void _rotate()
+    {
+        _body.MoveRotation(Quaternion.Lerp(_from.rotation, _to.rotation, Time.time * _speed));
+    }
+
+    public void Start()
+    {
+        _body = GetComponent<Rigidbody>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        var lane = other.GetComponent<Lane>();
+
+        if (lane == null)
+            return;
+
+        _from = lane.Start;
+        _to = lane.End;
+        _isTurning = true;
+    }
+
+    public void FixedUpdate()
+    {
+        if (_isTurning)
+            _rotate();
+
+        if (_to != null && transform.rotation == _to.rotation)
+            _isTurning = false;
+
+        _body.AddForce(transform.forward * _speed);
+    }
+}
