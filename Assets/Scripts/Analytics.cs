c2709b9a3da3d52fd7c316a9c590adf1e25165d2
Camelia <camelus.morari@gmail.com>
2018-06-13 17:15:36 +0300

    Hey! Am facut un script care face media tuturor vitezelor masinilor

---
+++ Assets/Scripts/Analytics.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptCami : MonoBehaviour {
+
+    [SerializeField] 
+    private Rigidbody Car; //luam viteza din rigibody
+    float suma; //suma tuturor vitezelor
+    float seconds;  //secundele
+    float average;  //media suma/timp
+        void Update()
+        {
+        suma += Car.velocity.magnitude; //car e vector 3 nu returneaza int sau float, deci folosim magnitude 
+        seconds += Time.deltaTime; //este o functie time care ia secunde
+        average = suma / seconds;
+        Debug.Log(average); //afisam in consola sa vedem daca merge
+        }
+    }
+
+
+
+
+
