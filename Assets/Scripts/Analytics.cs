ebb83d43c18ebe92d805bb5802c4e3f772b272b0
Camelia <camelus.morari@gmail.com>
2018-06-25 23:32:20 +0300

    total time, time standing still, car speed

--- Assets/Scripts/Analytics.cs
+++ Assets/Scripts/Analytics.cs
@@ -7,18 +7,38 @@ public class Analytics : MonoBehaviour {
 
     [SerializeField]
     private Text text;
+    [SerializeField]
+    private Text text1;
+
+    [SerializeField]
+    private Text text2;
+
+    [SerializeField]
+    private Text text3;
 
     [SerializeField] 
     private Rigidbody Car; //luam viteza din rigibody
     float suma;
     float seconds; 
-    float average;  
+    float average;
+    float still;
         void Update()
         {
         suma += Car.velocity.magnitude; //velocity e vector 3 nu returneaza int sau float, that's why we use magnitude 
         seconds += Time.deltaTime; 
         average = suma / seconds;
-        text.text = average.ToString();
+        text.text = "Av speed: " + average.ToString();
+        text1.text = "Speed:  " + Car.velocity.magnitude.ToString();
+        text2.text = "Total time: " + seconds.ToString();
+        if (Car.velocity.magnitude==0)
+        {
+            still += Time.deltaTime;
+            text3.text = "Standing still: " + still.ToString();
+        }
+        else
+        {
+            still = 0;
+        }
         }
 
     }
