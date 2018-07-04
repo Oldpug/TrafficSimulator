615f49c6f56ad85b07278c05c4c8f567f8e8e849
Imbuzan Radu <imbuzanradu@yahoo.com>
2018-07-04 11:37:37 +0300

    Update intersection to give random exit

---
+++ Assets/Scripts/IntersectionLane.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class IntersectionLane : Lane
+{
+
+    [SerializeField]
+    private List<Lane> possibleNextLanes = new List<Lane>();
+    private int nextLaneIndex;
+
+    private void Awake()
+    {
+        nextLaneIndex = Random.Range(0, possibleNextLanes.Count);
+    }
+
+    public override Transform End
+    {
+        get
+        {
+            return possibleNextLanes[nextLaneIndex].transform;
+        }
+    }
+
+    public override Lane Next
+    {
+        get
+        {
+            return possibleNextLanes[nextLaneIndex];
+        }
+    }
+}
