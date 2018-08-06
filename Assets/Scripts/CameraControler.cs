6c2358720ad6f4cfea383ab4b5be4178060bef6b
The Hearth <christiannitas@gmail.com>
2018-08-06 16:17:50 +0300

    Fix camera issues, improve control and added up and down features. Also Add a build mode to the camera where if activated, it will look at the map top down.

--- Assets/Scripts/CameraControler.cs
+++ Assets/Scripts/CameraControler.cs
@@ -5,26 +5,29 @@ using UnityEngine;
 public class CameraControler : MonoBehaviour {
 
     private Transform cameraControl; // the actual camera
-    private Transform cameraPivot; // the pivot of the camera
+    private Transform cameraRotationPivot; // the rotation pivot of the camera
+    private Transform cameraMovementPivot; // the movement pivot of the camera
     private Vector3 localRotation;
     private Vector3 localPosition;
     private Vector3 forwardMovementDirection;
     private Vector3 rightMovementDirection;
+    private Vector3 upwardMovementDirection;
+
+    private bool buildingMode = false;
+
+    private float lastZoomAmount;
+    private Quaternion lastRotation;
 
     [SerializeField]
     private float scrollSensitivity = 3f; // the sensitivity of the scroolwheel.
     [SerializeField]
     public float MouseSensitivity = 4f; // the sensitivity of the mouse. (might get changed when we let the user customize the work enviroment himself.
     [SerializeField]
-    private float rotationSensitiviy = 1f; //the sensitivity of the rotation
-    [SerializeField]
     private float minimumCloseness = -3; // the closest the camera can get to the pivot.
     [SerializeField]
     private float maximumCloseness = -80; // the farthest the camera can go away from the pivot.
     [SerializeField]
     private float orbitDampening = 10f; //camera orbiting speed dampener.
-    [SerializeField]
-    private float scrollDampening = 10; // scrolling speed dampener.
     [SerializeField]
     private float movementSpeedRatio = 1f; // camera horizontal and vertical movement speed.
     [SerializeField]
@@ -33,9 +36,10 @@ public class CameraControler : MonoBehaviour {
 	void Start ()
     {
         cameraControl = transform;
-        cameraPivot = transform.parent;
-        forwardMovementDirection = new Vector3(cameraPivot.transform.forward.x, cameraPivot.transform.forward.y, cameraPivot.transform.forward.z);
-        rightMovementDirection = new Vector3(cameraPivot.transform.right.x, cameraPivot.transform.right.y, cameraPivot.transform.right.z);
+        cameraRotationPivot = transform.parent;
+        cameraMovementPivot = cameraRotationPivot.transform.parent;
+        forwardMovementDirection = new Vector3(cameraMovementPivot.transform.forward.x, cameraMovementPivot.transform.forward.y, cameraMovementPivot.transform.forward.z);
+        rightMovementDirection = new Vector3(cameraMovementPivot.transform.right.x, cameraMovementPivot.transform.right.y, cameraMovementPivot.transform.right.z);
 	}
 	
 	void Update ()
@@ -49,12 +53,14 @@ public class CameraControler : MonoBehaviour {
                 cameraControl.transform.localPosition = new Vector3(cameraControl.transform.localPosition.x, cameraControl.transform.localPosition.y, scrollAmount);
         }
 
-        if(Input.GetMouseButton(1))
+        if(Input.GetMouseButton(1) && !buildingMode)
         {
             if(Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
             {
                 localRotation.x += Input.GetAxis("Mouse X") * MouseSensitivity;
                 localRotation.y -= Input.GetAxis("Mouse Y") * MouseSensitivity;
+
+                cameraMovementPivot.rotation = Quaternion.Euler(0, localRotation.x, 0);
 
                 if (localRotation.y < 0f)
                     localRotation.y = 0f;
@@ -62,17 +68,48 @@ public class CameraControler : MonoBehaviour {
                     localRotation.y = 90f;
 
                 Quaternion QT = Quaternion.Euler(localRotation.y, localRotation.x, 0);
-                cameraPivot.rotation = Quaternion.Lerp(this.cameraPivot.rotation, QT, orbitDampening);
+                cameraRotationPivot.rotation = Quaternion.Lerp(cameraRotationPivot.rotation, QT, orbitDampening);
+                
             }
         }
 
-        if(Input.GetAxis("Horizontal") != 0)
-            cameraPivot.transform.Translate(rightMovementDirection * Input.GetAxis("Horizontal") * movementSpeedRatio);
+        if(Input.GetKey(KeyCode.D))
+            cameraMovementPivot.Translate(rightMovementDirection * movementSpeedRatio);
 
-        if (Input.GetAxis("Vertical") != 0)
-            cameraPivot.transform.Translate(forwardMovementDirection * Input.GetAxis("Vertical") * movementSpeedRatio);
+        if (Input.GetKey(KeyCode.A))
+            cameraMovementPivot.Translate(rightMovementDirection * -1 *  movementSpeedRatio);
 
-        if (cameraPivot.position.y < 0)
-            cameraPivot.position = new Vector3(cameraPivot.position.x, lowestMapPoint, cameraPivot.position.z);
+        if (Input.GetKey(KeyCode.W))
+            cameraMovementPivot.Translate(forwardMovementDirection * movementSpeedRatio);
+
+        if(Input.GetKey(KeyCode.S))
+            cameraMovementPivot.Translate(forwardMovementDirection * -1 * movementSpeedRatio);
+
+        if (Input.GetKey(KeyCode.Q))
+            cameraMovementPivot.Translate(cameraMovementPivot.up * -1 * movementSpeedRatio);
+
+        if (Input.GetKey(KeyCode.E))
+            cameraMovementPivot.Translate(cameraMovementPivot.up * movementSpeedRatio);
+
+        if (cameraMovementPivot.position.y < 0)
+            cameraMovementPivot.position = new Vector3(cameraRotationPivot.position.x, lowestMapPoint, cameraRotationPivot.position.z);
+    }
+
+    public void SwitchMode()
+    {
+        if(buildingMode == false)
+        {
+            lastRotation = cameraRotationPivot.localRotation;
+            lastZoomAmount = cameraControl.localPosition.z;
+            cameraControl.localPosition = new Vector3(0, 0, maximumCloseness);
+            cameraRotationPivot.localRotation = Quaternion.Euler(90, 0, 0);
+            cameraMovementPivot.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            cameraRotationPivot.localRotation = lastRotation;
+            cameraControl.localPosition = new Vector3(0, 0, lastZoomAmount);
+        }
+        buildingMode = !buildingMode;
     }
 }
