73e88029183d00d2c7014814cfc97b396f24627a
Camelia <camelus.morari@gmail.com>
2018-08-09 12:24:33 +0300

    fixed bugs; when you click on a car it shows its speed; modified the panel

--- Assets/Scripts/Analytics.cs
+++ Assets/Scripts/Analytics.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -33,9 +34,7 @@ public class Analytics : MonoBehaviour {
 
     [SerializeField]
     private Text text9;
-
-    [SerializeField] 
-    private Rigidbody car; //luam viteza din rigibody
+    
 
     float suma;
     float seconds; 
@@ -48,18 +47,22 @@ public class Analytics : MonoBehaviour {
     private void Start()
     {
         StartCoroutine(Watch());
-
-        var cars = FindObjectsOfType<Car>();
+        cars = FindObjectsOfType<Car>();
     }
 
     private IEnumerator Watch()
     {
-        while (true)
+        foreach (Car car in cars)
         {
-            suma += car.velocity.magnitude;
-            yield return new WaitForSeconds(1);
+            while (true)
+            {
+                suma += car.Velocity;
+                yield return new WaitForSeconds(1);
+            }
         }
     }
+   
+
     void Update()
         {
 
@@ -70,44 +73,53 @@ public class Analytics : MonoBehaviour {
 
         foreach (Car car in cars) //average speed for all the cars
         {
-          /////  suma += car.velocity.magnitude;
+            suma += car.Velocity;
+            if (car.Velocity == 0)
+            {
+                still += Time.deltaTime;
+                finaltimestill += Time.deltaTime;
+
+                text3.text = "Standing still: ";
+                text8.text = "0";
+                text8.text = still.ToString("0.##");
+            }
+            else
+            {
+                still = 0;
+            }
+
         }
+        
         average = suma / cars.Length;
 
             text.text = "Average speed: ";
             text5.text = "0";
             text5.text = average.ToString("0.##");
 
-            text1.text = "Speed:  ";
-            text6.text = "0";
-            text6.text= car.velocity.magnitude.ToString("0.##");
-
             text2.text = "Total time: ";
             text7.text = "0";
             text7.text = seconds.ToString("0.##");
 
-            if (car.velocity.magnitude==0)
-            {
-                still += Time.deltaTime;
-                finaltimestill += Time.deltaTime;
-
-            text3.text = "Standing still: ";
-            text8.text = "0";
-            text8.text = still.ToString("0.##");
-            }
-            else
-            {
-                still = 0;
-            }
         // finaltimestill = Mathf.Round(finaltimestill * 100f) / 100f;
 
         text4.text = "Total time still: ";
         text9.text = "0";
         text9.text = finaltimestill.ToString("0.##");
 
-        }
+       }
 
+    void OnMouseDown()
+    {
+        // this object was clicked - do something
+        foreach (Car car in cars)
+        {
+            text1.text = "Speed:  ";
+            text6.text = "0";
+            text6.text = car.Velocity.ToString("0.##");
+        }
     }
+
+}
 
 
 
