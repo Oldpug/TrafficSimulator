b295edbba2a3f885872e21f3e31a4deae539f96a
Camelia <camelus.morari@gmail.com>
2018-08-21 20:07:32 +0300

    fixed Car velocity property

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,131 +1,142 @@
 ﻿using UnityEngine;
 
-public class Car : MonoBehaviour {
-  [SerializeField]
-  private float maxSpeed = 2f;
+public class Car : MonoBehaviour
+{
+    private const float EPSILON = 0.01f;
 
-  [SerializeField]
-  private float brakingSpeed = 2f;
+    [SerializeField]
+    private float maxSpeed = 2f;
 
-  [SerializeField]
-  private float viewDistance = 1.5f;
+    [SerializeField]
+    private float brakingSpeed = 2f;
 
-  [SerializeField]
-  private Lane lane;
+    [SerializeField]
+    private float viewDistance = 1.5f;
 
-  private Rigidbody body;
+    [SerializeField]
+    private Lane lane;
 
-  private Driver driver;
+    private Rigidbody body;
 
-  private float speed;
+    private Driver driver;
 
-  private bool isTurning;
+    private float speed;
 
-  private float laneLength;
+    private bool isTurning;
 
-  private Vector3 laneBeginFwd;
+    private float laneLength;
 
-  private Vector3 laneBeginPos;
+    private Vector3 laneBeginFwd;
 
-  private Vector3 lastFramePos;
+    private Vector3 laneBeginPos;
+
+    private Vector3 lastFramePos;
 
     private void Start()
     {
         lastFramePos = transform.position;
     }
 
-    public float Velocity {
-    get {
-      float x = Vector3.Distance(lastFramePos, transform.position);
+    public float Velocity { get; private set; }
 
-            return x > 0.1f ? x : 0;
-    }
-  }
+    public bool IsFacingObstacle
+    {
+        get
+        {
+            RaycastHit hit;
+            bool isFacingObstacle = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, viewDistance, ~0, QueryTriggerInteraction.Collide);
 
-  public bool IsFacingObstacle {
-    get {
-      RaycastHit hit;
-      bool isFacingObstacle = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, viewDistance, ~0, QueryTriggerInteraction.Collide);
+            if (isFacingObstacle)
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            else
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
 
-      if (isFacingObstacle)
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-      else
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-
-      return isFacingObstacle;
-    }
-  }
-
-  private void RecordMovement() {
-    lastFramePos = transform.position;
-  }
-
-  private void InitLane() {
-    if (lane == null)
-      return;
-
-    if (lane.End == null) {
-      lane = null;
-      return;
+            return isFacingObstacle;
+        }
     }
 
-    laneBeginPos = transform.position;
-    laneBeginFwd = transform.forward;
-
-    laneLength = Vector3.Distance(transform.position, lane.End.position);
-
-    isTurning = transform.forward != lane.End.forward;
-  }
-
-  private void AdvanceLane() {
-    transform.position = lane.End.position;
-    transform.rotation = lane.End.rotation;
-
-    lane = lane.Next ?? driver.GetDirection();
-    InitLane();
-  }
-
-  private void Move() {
-    if (lane == null)
-      return;
-
-    var currentDistance = Vector3.Distance(laneBeginPos, transform.position);
-
-    if (isTurning) {
-      var dir = Vector3.Lerp(laneBeginFwd, lane.End.forward, currentDistance / laneLength);
-      body.MoveRotation(Quaternion.LookRotation(dir, Vector3.up));
+    private void RecordMovement()
+    {
+        float x = Vector3.Distance(lastFramePos, transform.position);
+        Velocity =  x > EPSILON ? x : 0;
+        lastFramePos = transform.position;
     }
 
-    body.MovePosition(transform.position + transform.forward * speed * Time.fixedDeltaTime);
+    private void InitLane()
+    {
+        if (lane == null)
+            return;
 
-    if (Mathf.Abs(laneLength - currentDistance) < 0.1f)
-      AdvanceLane();
-  }
+        if (lane.End == null)
+        {
+            lane = null;
+            return;
+        }
 
-  private void Brake() {
-    speed = Mathf.Max(speed - brakingSpeed * Time.fixedDeltaTime, 0);
-  }
+        laneBeginPos = transform.position;
+        laneBeginFwd = transform.forward;
 
-  private void Accelerate() {
-    speed = Mathf.Min(speed + brakingSpeed * Time.fixedDeltaTime, maxSpeed);
-  }
+        laneLength = Vector3.Distance(transform.position, lane.End.position);
 
-  private void Awake() {
-    body = GetComponent<Rigidbody>();
-    driver = GetComponent<Driver>();
+        isTurning = transform.forward != lane.End.forward;
+    }
 
-    speed = maxSpeed;
-    InitLane();
-  }
+    private void AdvanceLane()
+    {
+        transform.position = lane.End.position;
+        transform.rotation = lane.End.rotation;
 
-  private void FixedUpdate() {
-    RecordMovement();
+        lane = lane.Next ?? driver.GetDirection();
+        InitLane();
+    }
 
-    if (IsFacingObstacle)
-      Brake();
-    else
-      Accelerate();
+    private void Move()
+    {
+        if (lane == null)
+            return;
 
-    Move();
-  }
+        var currentDistance = Vector3.Distance(laneBeginPos, transform.position);
+
+        if (isTurning)
+        {
+            var dir = Vector3.Lerp(laneBeginFwd, lane.End.forward, currentDistance / laneLength);
+            body.MoveRotation(Quaternion.LookRotation(dir, Vector3.up));
+        }
+
+        body.MovePosition(transform.position + transform.forward * speed * Time.fixedDeltaTime);
+
+        if (Mathf.Abs(laneLength - currentDistance) < EPSILON)
+            AdvanceLane();
+    }
+
+    private void Brake()
+    {
+        speed = Mathf.Max(speed - brakingSpeed * Time.fixedDeltaTime, 0);
+    }
+
+    private void Accelerate()
+    {
+        speed = Mathf.Min(speed + brakingSpeed * Time.fixedDeltaTime, maxSpeed);
+    }
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        driver = GetComponent<Driver>();
+
+        speed = maxSpeed;
+        InitLane();
+    }
+
+    private void FixedUpdate()
+    {
+        RecordMovement();
+
+        if (IsFacingObstacle)
+            Brake();
+        else
+            Accelerate();
+
+        Move();
+    }
 }
