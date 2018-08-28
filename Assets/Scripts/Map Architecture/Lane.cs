45911c45a3b97769dfa8785798a65acfee0a9cb3
RIA\amorari <amorari@riasolutionsgroup.com>
2018-08-28 17:11:13 +0300

    Revert "Add 30, 45, and 60 degree turn prefabs"
    This reverts commit c86148976b9202e28f4c7a5c896f2168086594cf.

--- Assets/Scripts/Map Architecture/Lane.cs
+++ Assets/Scripts/Map Architecture/Lane.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 
-public abstract class Lane : MonoBehaviour {
-  public abstract Transform End { get; }
+public abstract class Lane : MonoBehaviour
+{
+    public abstract Transform End { get; }
 
-  public abstract Lane Next { get; }
+    public abstract Lane Next { get; }
+
+    public abstract void SetNextLane(Lane lane);
 }
