782607c0e3891b6e79963723ca40f7f9cd2afade
Cristi <christiannitas@gmail.com>
2018-08-29 17:40:15 +0300

    Add linker class and the interface to linkers.

---
+++ Assets/Scripts/Simulation Control/Linker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linker : MonoBehaviour
+{
+    [SerializeField]
+    Lane primary;
+
+    public Lane Primary
+    {
+        get
+        {
+            return primary;
+        }
+    }
+
+    [SerializeField]
+    private Lane secondary;
+
+    public Lane Secondary
+    {
+        get
+        {
+            return secondary;
+        }
+    }
+
+    [SerializeField]
+    private Transform linkingPoint;
+
+    [SerializeField]
+    public Transform oldestParent;
+
+    [SerializeField]
+    public Transform road;
+
+    public void Link(Linker son)
+    {
+        primary.Next = son.Secondary;
+        son.Primary.Next = secondary;
+
+        son.road.transform.position = linkingPoint.position + (son.road.position - son.linkingPoint.position);
+    }
+
+}
