b0b20b2039298bea3fe7ab0d0219f01010206269
Cristi <christiannitas@gmail.com>
2018-08-31 19:20:50 +0300

    Add InterfaceManager and RoadStructure script. Remove Building Manager.

---
+++ Assets/Scripts/Simulation Control/InterfaceManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class InterfaceManager : MonoBehaviour
+{
+    public bool buildMode;
+
+    [SerializeField]
+    CameraControler cameraControl;
+
+    [SerializeField]
+    GameObject buildPanel;
+    [SerializeField]
+    GameObject analyticsPanel;
+    Linker[] linkers;
+
+    private void Start()
+    {
+        cameraControl = FindObjectOfType<CameraControler>();
+        linkers = FindObjectsOfType<Linker>();
+        foreach (Linker linker in linkers)
+        {
+            linker.gameObject.SetActive(false);
+        }
+    }
+
+    public void SpawnRoad(RoadStructure road)
+    {
+        Instantiate(road);
+        road.OnMouseDown();
+        road.interfaceManager = this;
+        road.transform.parent = null;
+    }
+
+    public void EnableRoadPanel(Transform road)
+    {
+
+    }
+
+    public void ToggleBuildMode()
+    {
+        cameraControl.SwitchMode();
+        if(buildMode)
+        {
+            buildPanel.SetActive(false);
+        }
+        else
+        {
+            buildPanel.SetActive(true);
+            analyticsPanel.SetActive(false);
+        }
+        
+        buildMode = !buildMode;
+        ToggleLinkingInterface(buildMode);
+
+    }
+    
+    public void ToggleLinkingInterface(bool value)
+    {
+        if(!value)
+        {
+            linkers = FindObjectsOfType<Linker>();
+        }
+        foreach(Linker linker in linkers)
+        {
+            linker.gameObject.SetActive(value);
+        }
+    }
+}
---
+++ Assets/Scripts/Simulation Control/RoadStructure.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadStructure : MonoBehaviour {
+
+    [SerializeField]
+    public Transform linkingInterface;
+
+    public InterfaceManager interfaceManager;
+
+    private Vector3 draggingOffset;
+    private Vector3 screenPoint;
+
+    public void OnMouseDown()
+    {
+        interfaceManager.EnableRoadPanel(transform);
+        if(interfaceManager.buildMode)
+        {
+            screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+            draggingOffset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        }
+    }
+
+    public void OnMouseDrag()
+    {
+        if (interfaceManager.buildMode)
+        {
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + draggingOffset;
+            transform.position = curPosition;
+        }
+    }
+}
