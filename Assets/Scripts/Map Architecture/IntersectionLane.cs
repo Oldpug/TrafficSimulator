bf53fc9b1db595feefa9fb3b8844d1b51fb09424
Alex Ionescu <alxi.2001@gmail.com>
2018-09-11 12:19:14 +0300

    Fix InterfaceLane not being serialized properly (Unity can't serialize arrays-of-arrays)

--- Assets/Scripts/Map Architecture/IntersectionLane.cs
+++ Assets/Scripts/Map Architecture/IntersectionLane.cs
@@ -2,10 +2,17 @@
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+[Serializable]
+public class IntersectionExit
+{
+    [SerializeField]
+    public BasicLane[] Lanes;
+}
+
 public class IntersectionLane : Lane
 {
     [SerializeField]
-    public BasicLane[][] Exits;
+    public IntersectionExit[] Exits;
 
     public override Transform End
     {
@@ -28,12 +35,12 @@ public class IntersectionLane : Lane
     public BasicLane GetRandomExit(Transform carPosition)
     {
         var exitIdx = Random.Range(0, Exits.Length);
-        var exit = Exits[exitIdx];
+        var exit = Exits[exitIdx].Lanes;
 
         while (carPosition.forward + exit[0].End.transform.forward == Vector3.zero)
         {
             exitIdx = Random.Range(0, Exits.Length);
-            exit = Exits[exitIdx];
+            exit = Exits[exitIdx].Lanes;
         }
 
         var lane = Random.Range(0, exit.Length);
