dd85ce15a2c7d89d5e6a9d311521e4ceb0f065b0
Cristi <christiannitas@gmail.com>
2018-09-17 03:31:13 +0300

    Create Spawner Prefab

--- Assets/Scripts/Simulation Control/CameraControler.cs
+++ Assets/Scripts/Simulation Control/CameraControler.cs
@@ -69,11 +69,6 @@ public class CameraControler : MonoBehaviour {
 
                 Quaternion QT = Quaternion.Euler(localRotation.y, localRotation.x, 0);
                 cameraRotationPivot.rotation = Quaternion.Lerp(cameraRotationPivot.rotation, QT, orbitDampening);
-                if (Input.GetKey(KeyCode.Q))
-                    cameraMovementPivot.Translate(cameraMovementPivot.up * -1 * movementSpeedRatio);
-
-                if (Input.GetKey(KeyCode.E))
-                    cameraMovementPivot.Translate(cameraMovementPivot.up * movementSpeedRatio);
             }
         }
 
@@ -89,7 +84,13 @@ public class CameraControler : MonoBehaviour {
         if(Input.GetKey(KeyCode.S))
             cameraMovementPivot.Translate(forwardMovementDirection * -1 * movementSpeedRatio);
 
-        
+        if (Input.GetKey(KeyCode.Q) && !buildingMode)
+            cameraMovementPivot.Translate(cameraMovementPivot.up * -1 * movementSpeedRatio);
+
+        if (Input.GetKey(KeyCode.E) && !buildingMode)
+            cameraMovementPivot.Translate(cameraMovementPivot.up * movementSpeedRatio);
+
+
 
         if (cameraMovementPivot.position.y < 0)
             cameraMovementPivot.position = new Vector3(cameraRotationPivot.position.x, lowestMapPoint, cameraRotationPivot.position.z);
