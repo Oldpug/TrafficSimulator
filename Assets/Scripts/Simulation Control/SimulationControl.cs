f5e92f67fac56c6d704e702e553f31aac4dd07b6
Alex Ionescu <alxi.2001@gmail.com>
2018-09-18 22:06:28 +0300

    Fix time scale display

--- Assets/Scripts/Simulation Control/SimulationControl.cs
+++ Assets/Scripts/Simulation Control/SimulationControl.cs
@@ -18,6 +18,7 @@ public class SimulationControl : MonoBehaviour
     public void Awake()
     {
         Time.timeScale = 0;
+        textTimeScale.text = "Speed x" + x;
     }
 
     public void PlayPause()
@@ -44,16 +45,18 @@ public class SimulationControl : MonoBehaviour
 
     public void SpeedUp()
     {
-        Time.timeScale *= 2;
-        if (Time.timeScale >= 100)
-            Time.timeScale = 100;
-        textTimeScale.text = Time.timeScale.ToString();
+        var timeScale = Mathf.Min(Time.timeScale * 2, 64);
+
+        Time.timeScale = timeScale;
+        textTimeScale.text = "Speed x" + timeScale.ToString();
     }
 
     public void SlowDown()
     {
-        Time.timeScale /= 2;
-        textTimeScale.text = Time.timeScale.ToString();
+        var timeScale = Mathf.Max(Time.timeScale / 2, 1);
+
+        Time.timeScale = timeScale;
+        textTimeScale.text = "Speed x" + timeScale.ToString();
     }
 
 }
