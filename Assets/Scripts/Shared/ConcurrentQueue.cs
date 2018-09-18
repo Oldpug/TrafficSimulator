cfcd31865028b6fba3cf4c38deb0a415876be26b
Alex Ionescu <alxi.2001@gmail.com>
2018-09-18 18:36:19 +0300

    Format PriorityQueue.cs

--- Assets/Scripts/Shared/ConcurrentQueue.cs
+++ Assets/Scripts/Shared/ConcurrentQueue.cs
@@ -1,47 +1,52 @@
 ﻿using System.Collections.Generic;
 
-public sealed class ConcurrentQueue<T> {
-  readonly object syncRoot = new object();
+public sealed class ConcurrentQueue<T>
+{
+    readonly object syncRoot = new object();
 
-  readonly Queue<T> queue = new Queue<T>();
+    readonly Queue<T> queue = new Queue<T>();
 
-  public object SyncRoot {
-    get {
-      return syncRoot;
+    public object SyncRoot
+    {
+        get
+        {
+            return syncRoot;
+        }
     }
-  }
 
-  public int Count {
-    get {
-      lock (syncRoot) {
-        return queue.Count;
-      }
+    public int Count
+    {
+        get
+        {
+            lock (syncRoot)
+                return queue.Count;
+        }
     }
-  }
 
-  public bool IsEmpty {
-    get {
-      lock (syncRoot) {
-        return queue.Count == 0;
-      }
+    public bool IsEmpty
+    {
+        get
+        {
+            lock (syncRoot)
+                return queue.Count == 0;
+        }
     }
-  }
 
-  public T Dequeue() {
-    lock (syncRoot) {
-      return queue.Dequeue();
+    public T Dequeue()
+    {
+        lock (syncRoot)
+            return queue.Dequeue();
     }
-  }
 
-  public void Enqueue(T item) {
-    lock (syncRoot) {
-      queue.Enqueue(item);
+    public void Enqueue(T item)
+    {
+        lock (syncRoot)
+            queue.Enqueue(item);
     }
-  }
 
-  public void Clear() {
-    lock (syncRoot) {
-      queue.Clear();
+    public void Clear()
+    {
+        lock (syncRoot)
+            queue.Clear();
     }
-  }
 }
